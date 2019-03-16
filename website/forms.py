8c8a4812221c06a24fb44967b850a3b7c1863d57
saikatghosh1998 <saikatghosh449@gmail.com>
2019-03-17 01:26:47 +0530

    Data showing up in List

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class VideoForm(forms.Form):
+class yearSearch(forms.Form):
     videoname = forms.CharField(max_length=20,
         widget= forms.TextInput(attrs={
             'class': 'form-control',
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import VideoForm
+from .forms import yearSearch
 import sys
 import requests
 from bs4 import BeautifulSoup as bs
@@ -8,28 +8,33 @@ from bs4 import BeautifulSoup as bs
 # Create your views here.
 def search(request):
     if request.method == 'POST':
-        form = VideoForm(request.POST)
+        form = yearSearch(request.POST)
         videotitle=request.POST['videoname']
 
         url = "https://en.wikipedia.org/wiki/"
         year = videotitle
         url += year + "_in_India"
-        r = requests.get(url)  # sending request to the html document
-        soup = bs(r.content, 'lxml')  # parsing the html doc
-        tagDoc = soup.find("div", {"class": "mw-parser-output"}) 
-        details=tagDoc.text    
-        context = {'form': details}
+        html = requests.get(url)
+        soup = bs(html.content,'lxml')
+        container = soup.find('div', {'class':'mw-content-ltr'})
+        alList = container.findAll('ul')[7]
+        data = alList.select('li')
+        x=[]
+        for d in data:
+            x.append(d.text.strip())
+        
+        context = {'detail': x}
 
     return render(request,'website/search.html',context)
 
 def home(request):
     if request.method == 'POST':
-        form = VideoForm(request.POST)
+        form = yearSearch(request.POST)
 
         if form.is_valid():
             return redirect('search')
     else:
-        form = VideoForm()
+        form = yearSearch()
 
     context = {'form':form}
 
